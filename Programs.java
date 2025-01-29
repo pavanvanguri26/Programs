d188c36ff97c4ef645280e1fd7405c38bfd904ca
pavanvanguri26 <pavankumaryadavvanguri630@gmail.com>
2025-01-29 22:08:42 +0530

    Add files via upload

---
+++ Programs.java
@@ -0,0 +1,63 @@
+// public class Programs {
+//     public static void main(String[] args) {
+//         char number=65;
+//         System.out.println(number);
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Programs {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         char ch=sc.next.charAt(0);
+//         System.out.println(int(ch));
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Programs {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         float length=sc.nextFloat();
+//         float breadth=sc.nextFloat();
+//         sc.close();
+//         System.out.printf("area of rectangle="+length * breadth);
+//     }
+// }
+
+// public class Programs{
+//     public static void main(String[] args) {
+//         int a=45;
+//         int b=13;
+//         System.out.println(("Addition:")+(a+b));
+//         System.out.println(("Substraction:") +( a-b));
+//         System.out.println(("Multiplication:")+(a*b));
+//         System.out.println(("Division:")+(a/b));
+//     }
+// }
+
+// public class Programs{
+//     public static void main(String[] args) {
+//         int a=100;
+//         int b=4;
+//         System.out.println(a+b);
+//         System.out.println(a-b);
+//         System.out.println(a*b);
+//         System.out.println(a/b);
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Programs{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         int c;
+//         c=a;
+//         a=b;
+//         b=c;
+//         System.out.println("a="+a);
+//         System.out.println("b="+b);
+//     }
+// }
